e898f9334742744bc30e248449b4160a3c3f007e
Edgar Eduardo Reyna Aguilar <s204406@ahmsabmva.ahmsab.net>
2020-02-17 18:55:10 -0600

    update ionic bases

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -7,9 +8,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  constructor(public alertCtrl: AlertController) { }
+
+  titulo: string;
 
   ngOnInit() {
+  }
+
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'Alert',
+      backdropDismiss:false,
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Boton Cancelar');
+          }
+        },
+        {
+          text: 'Ok',
+          handler: (blah) => {
+            console.log('Boton OK');
+          }
+        },
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async presentAlertPrompt() {
+    const alert = await this.alertCtrl.create({
+      header: 'Input',
+      subHeader: 'Ingrese su nombre:',
+      backdropDismiss:false,
+      inputs: [
+        {
+          name: 'txtNombre',
+          type: 'text',
+          placeholder: 'Nombre'
+        }       
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'OK',
+          handler: (datos) => {
+            console.log('Confirm Ok ',datos);
+            this.titulo=datos.txtNombre;
+            
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -18,6 +18,21 @@ export class InicioPage implements OnInit {
       name:"Alert",
       redirectTo:"/alert"
     },
+    {
+      icon:"beaker",
+      name:"Avatar",
+      redirectTo:"/avatar"
+    },
+    {
+      icon:"radio-button-on",
+      name:"Botones y router",
+      redirectTo:"/botones"
+    },
+    {
+      icon:"card",
+      name:"Cards",
+      redirectTo:"/card"
+    },
   ];
 
   constructor() { }
